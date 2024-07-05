d5c4630a853ce6764b777f786fc55d682301e3fd
Sakin790 <sakinislam79@gmail.com>
2024-07-05 16:08:19 +0530

    added response Modle

--- Express/index.js
+++ Express/index.js
@@ -1,12 +1,23 @@
 import express from "express"
 const app = express()
+
+
+
+
+
+
 const PORT = 8080
-app.listen(PORT, () => {
-    console.log(`Server is Running At Port ${PORT}`);
-})
+
 
 app.get("/", (req, res) => {
+    console.log(`Header is ${req.headers}`);
     res.send({
         message: "Server is Working"
     })
+})
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server is Running At Port ${PORT}`);
 })
